173ef6f18df59b908b320c640229dc520ddb9abe
Dimitriy Tsinis <dimitriyt@silicom.co.il>
2020-10-23 13:03:41 +0300

    Register DB Tables
    The tables for the basic register system.

---
+++ RMData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] NVARCHAR(128) NOT NULL PRIMARY KEY, 
+    [FirstName] VARCHAR(50) NOT NULL, 
+    [LastName] VARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(256) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
