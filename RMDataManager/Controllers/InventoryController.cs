bdae46aa8cb4d1179c3a690e5accefa160e11144
Dimitriy Tsinis <dimitriyt@silicom.co.il>
2021-01-23 12:08:30 +0200

    Add User Roles
    Add authorisation by role to the endpoints.

--- RMDataManager/Controllers/InventoryController.cs
+++ RMDataManager/Controllers/InventoryController.cs
@@ -12,12 +12,15 @@ namespace RMDataManager.Controllers
     [Authorize]
     public class InventoryController : ApiController
     {
+        [Authorize(Roles = "Manager,Admin")] //Comma meaning "OR" relationship. Or Manager or Admin can Get this info
         public List<InventoryModel> Get()
         {
             InventoryData data = new InventoryData();
             return data.GetInventory();
         }
 
+        //[Authorize(Roles = "WarehouseWorker")] Two lines meaning "AND". WarehouseWorker and Admin can Post the info
+        [Authorize(Roles = "Admin")]
         public void Post(InventoryModel item)
         {
             InventoryData data = new InventoryData();
