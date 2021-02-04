93c8228418796b4fe61729d3173378b8a714d7a2
Dimitriy Tsinis <dimitriyt@silicom.co.il>
2021-02-04 10:22:33 +0200

    Added New API and Moved Over Functionality

---
+++ RMApi/Controllers/InventoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class InventoryController : ControllerBase
+    {
+        [Authorize(Roles = "Manager,Admin")] //Coma meaning "OR" relationship. Or Manager or Admin can Get this info
+        public List<InventoryModel> Get()
+        {
+            InventoryData data = new InventoryData();
+            return data.GetInventory();
+        }
+
+        //[Authorize(Roles = "WarehouseWorker")] Two lines meaning "AND". WarehouseWorker and Admin can Post the info
+        [Authorize(Roles = "Admin")]
+        public void Post(InventoryModel item)
+        {
+            InventoryData data = new InventoryData();
+            data.SaveInventoryRecord(item);
+        }
+    }
+}
